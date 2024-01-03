e649602b824d06a7b07a388618091eab96cbe239
Radu Ciurca <raduciurca@gmail.com>
2024-01-03 11:40:02 +0200

    Add To library functionality for user

---
+++ Pages/User/Releases/Details.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProiectMPD.Data;
+using ProiectMPD.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace ProiectMPD.Pages.User.Releases
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ProiectMPD.Data.ApplicationDbContext _context;
+        public bool IsInLibrary { get; set; }
+
+
+        public DetailsModel(ProiectMPD.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Release Release { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var release = await _context.Releases
+                .Include(r=>r.Artist)
+                .Include(r=>r.Songs)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (release == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Release = release;
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // Get current user's ID
+            IsInLibrary = await _context.MusicLibraries
+                                .Where(l => l.UserId == userId)
+                                .SelectMany(l => l.Releases)
+                                .AnyAsync(r => r.ID == id);
+            return Page();
+        }
+    }
+}
---
+++ Pages/User/Releases/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProiectMPD.Data;
+using ProiectMPD.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace ProiectMPD.Pages.User.Releases
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProiectMPD.Data.ApplicationDbContext _context;
+
+        public IndexModel(ProiectMPD.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Release> Release { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Release = await _context.Releases
+                .Include(r => r.Artist).ToListAsync();
+        }
+        public async Task<IActionResult> OnPostAddToLibraryAsync(int releaseId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // Get current user's ID
+            var library = await _context.MusicLibraries.FirstOrDefaultAsync(l => l.UserId == userId);
+            
+            if (library.Releases == null)
+            {
+                library.Releases = new List<Release>();
+            }
+
+            if (!library.Releases.Any(r => r.ID == releaseId))
+            {
+                var releaseToAdd = await _context.Releases.FindAsync(releaseId);
+                library.Releases.Add(releaseToAdd);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
