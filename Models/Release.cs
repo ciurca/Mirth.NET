c7f918e0e6ec94213e3c3646f1f83109a396c65b
Radu Ciurca <raduciurca@gmail.com>
2024-01-05 19:01:51 +0200

    Modificare titlu reviews

--- Models/Release.cs
+++ Models/Release.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyModel;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProiectMPD.Models
 {
@@ -6,10 +8,13 @@ namespace ProiectMPD.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [RegularExpression(@"^(Single|EP|Album|Mixtape)$", ErrorMessage = "Enter a valid Type. Types allowed: Single, EP, Album or Mixtape")]
         public string Type { get; set; }
         public string? Language { get; set; }
+        [Url(ErrorMessage = "Enter a valid URL pointing to the Artwork of the release.")]
         public string? Artwork { get; set; }
         public string? Label { get; set; }
+        [RegularExpression(@"^[0-9]{4}$", ErrorMessage = "Enter a valid Year.")]
         public int? Year { get; set; }
         // Navigation properties for many-to-many relationship
         public ICollection<ReleaseGenre>? ReleaseGenres { get; set; }
--- Models/Song.cs
+++ Models/Song.cs
@@ -1,9 +1,12 @@
-﻿namespace ProiectMPD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProiectMPD.Models
 {
     public class Song
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [RegularExpression(@"^([0-5]?\d):([0-5]?\d):([0-5]?\d)$", ErrorMessage = "Enter a valid Length. Format: hh:mm:ss")]
         public TimeSpan Length { get; set; }
         // Assuming Song to Release is a one-to-many relationship
         public int? ReleaseID { get; set; } // Foreign key
