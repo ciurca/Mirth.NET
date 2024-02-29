4d2f428d7b6924e6832aa12c7e60d3bef21b3f0e
Radu Ciurca <raduciurca@gmail.com>
2024-02-29 10:34:38 +0200

    Azure Publishing

--- Program.cs
+++ Program.cs
@@ -20,9 +20,20 @@ builder.Services.AddRazorPages(options =>
     options.Conventions.AuthorizeFolder("/User", "UserPolicy");
 });
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")=="Production")
+{
+    var connectionStringProd = builder.Configuration.GetConnectionString("ProiectMDPContextProd") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionStringProd));
+} else
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+}
+// Commented due to possible startup issues (500.30)
+// builder.Services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
